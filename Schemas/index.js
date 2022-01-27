dce10fc44de01b117a112327c1912771dec5c101
Ajayi <eajayi@deloitte.com.ng>
2022-01-27 16:20:46 +0100

    Refactored index.js file

---
+++ Schemas/index.js
@@ -0,0 +1,56 @@
+const graphql = require('graphql');
+const  { 
+    GraphQLObjectType,
+    GraphQLSchema, 
+    GraphQLInt, 
+    GraphQLString,
+    GraphQLList
+} = graphql;
+const UserType = require('./TypeDefs/UserType');
+const userData = require('../mock_data.json');
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        getAllUsers: {
+            type: new GraphQLList(UserType),
+            args: { id: { type: GraphQLInt} },
+            resolve(parent, args) {
+                return userData
+            }
+        }
+    }
+})
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser: {
+            type: UserType,
+            args: {
+                firstName: { type: GraphQLString},
+                lastName: { type: GraphQLString},
+                email: { type: GraphQLString},
+                password: { type: GraphQLString}
+            },
+            resolve(parent, args) {
+                userData.push({
+                    id: userData.length + 1,
+                    firstName: args.firstName,
+                    lastName: args.lastName,
+                    email: args.email,
+                    password: args.password
+                })
+    
+                return args
+            }
+        }
+        
+    }
+});
+
+const schema = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
+
+module.exports = schema;
